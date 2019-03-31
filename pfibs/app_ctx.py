516f6b45a976dc6ab877dda0551a5c685f1f1637
jchang <justin.chang@nrel.gov>
2019-03-31 14:15:43 -0600

    deleted unnecessary file

--- pfibs/app_ctx.py
+++
@@ -1,8 +0,0 @@
-class AppCtx(object):
-    def __init__(self,ctx):
-        if not isinstance(ctx,dict):
-            raise TypeError("Application context must be of type dict()")
-        else:
-            self.appctx = ctx
-    def content(self):
-        return self.appctx
