bb5ed24d5d5b78a47b57d0e0dd3bdbbbdba8bbd0
jefalon <jeffery.allen@nrel.gov>
2019-04-03 11:36:24 -0600

    changed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name ="pfibs",
-      version="beta",
+      version="2018.1.0_0.1",
       author="Jeffery Allen, Justin Chang",
       author_email="jallen@nrel.gov",
       url="https://github.nrel.gov/jallen/block_solve",
